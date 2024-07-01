5fcb7a2b71a18306e6bdf050284b900747b2d6e7
kitkatbr <91047095+kitkatbr@users.noreply.github.com>
2024-07-01 18:37:24 -0300

    update

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,3 +1,3 @@
-const medusaUrl = __MEDUSA_BACKEND_URL__ || "//backend-server-production-f1d6.up.railway.app/"
+const medusaUrl = __MEDUSA_BACKEND_URL__ || "//backend-server-production-6e97.up.railway.app"
 
 export { medusaUrl }
